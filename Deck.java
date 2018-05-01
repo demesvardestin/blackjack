733a9fb0d8c7cb9c1320e4dcd3a5940fe73b17c9
baksha97 <baksha97@gmail.com>
2018-04-30 21:11:58 -0400

    update

--- Deck.java
+++ Deck.java
@@ -3,17 +3,9 @@ import java.util.Arrays;
 import java.util.ArrayList;
 
 public class Deck {
-  public Deck() {
-    ArrayList<Element> arrayList = new ArrayList<Element>(Arrays.asList([1,2,3,4,5,6,7,8,9,10]));
-  }
+  private ArrayList<Integer> cards;
   
-  public ArrayList Fill() {
-    ArrayList<Integer> deck = new ArrayList<Integer>();
-    int i = 1;
-    do {
-      deck.add(i);
-      i += 1;
-    } while(i < 11);
-    return deck;
+  public Deck() {
+    cards = new ArrayList<Integer>(Arrays.asList([1,2,3,4,5,6,7,8,9,10]));
   }
 }
